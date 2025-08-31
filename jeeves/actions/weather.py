9d483d9e7cfc525f4fc6b6541124125d96703e0f
Ramon Rodrigues <ramonfrombr@gmail.com>
2025-08-31 09:42:08 -0300

    Implemented weather action

--- jeeves/actions/weather.py
+++ jeeves/actions/weather.py
@@ -1,2 +1,38 @@
-async def weather_action(message, metadata):
-    return "Weather message"
+import requests
+from quart import current_app
+from urllib.parse import quote as urlquote
+
+
+def kelvin_to_celcius(kelvin):
+    return int(kelvin - 273.15)
+
+
+async def process_weather_response(weather_data):
+    temperature = kelvin_to_celcius(weather_data["main"]["temp"])
+    feels_like = kelvin_to_celcius(weather_data["main"]["feels_like"])
+    return f"The temperature is {temperature}℃ and it feels like {feels_like}℃"
+
+
+async def fetch_weather(location):
+    text = urlquote(location)
+
+    url = current_app.config["WEATHER_URL"].format(
+        text=text, token=current_app.config["WEATHER_TOKEN"])
+
+    response = requests.get(url)
+    response.raise_for_status()
+    return process_weather_response(response.json())
+
+
+async def weather_action(text, metadata):
+    # Turn the text into something that's probably a location
+    # Allow:
+    # weather in location
+    # weather location
+    if not text:
+        # TODO look up user.
+        location = "London, UK"
+    else:
+        location = text.replace("weather", "").replace("in", "").strip()
+
+    return await fetch_weather(location)
